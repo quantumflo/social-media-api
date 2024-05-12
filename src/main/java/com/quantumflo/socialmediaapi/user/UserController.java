4ff9c1eec14e5e339f2267be972f2b597c7b3977
shashank <shashankhebbarm@gmail.com>
2024-05-12 17:21:52 +0530

    Content negotiation and i18n

--- src/main/java/com/quantumflo/socialmediaapi/user/UserController.java
+++ src/main/java/com/quantumflo/socialmediaapi/user/UserController.java
@@ -1,20 +1,38 @@
 package com.quantumflo.socialmediaapi.user;
 
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.Predicate;
 
 @RestController
 public class UserController {
     private UserDaoService service;
+    private MessageSource messageSource;
 
-    public UserController(UserDaoService service) {
+    public UserController(UserDaoService service, MessageSource messageSource) {
+
         this.service = service;
+        this.messageSource =  messageSource;
+    }
+
+    @GetMapping(path = "/hello-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
+
     }
     @GetMapping("/users")
     public List<User> fetchUsers() {
